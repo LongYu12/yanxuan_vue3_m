bc10c0fa41959bf556d00f949f88ceb0fa121261
龙雨枫 <553374668@qq.com>
2023-02-28 22:16:37 +0800

    项目打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './', // 打包路径设置
+  // 打包路径设置
+  base: process.env.NODE_ENV === 'production' ? './' : '/', 
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
